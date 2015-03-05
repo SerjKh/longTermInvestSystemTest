8e027931b55676b3b2b4d60bf9adcc1f3dbb8f51
SerjKh <serjkh@gmail.com>
2015-03-05 09:05:52 +0200

    DB i/f expanded with new functions. Main with different options added. First working database populate with companies

--- ltist.py
+++ ltist.py
@@ -1,10 +1,55 @@
 import argparse
- 
-parser = argparse.ArgumentParser(description='LTIST - stands for long term investment system test')
-parser.add_argument('-p','--populateCompanies', help='Optional parameter, if specified, the script will use the list to populate the fundamentals database')
-parser.add_argument('-a','--addDMDRNReports', help='Optional parameter, if specified, the script will add reports taken from DMDRN database on each company existing in fundamentals database')
-args = parser.parse_args()
- 
-## show values ##
-print ("populateCompanies option: %s" % args.populateCompanies )
-print ("addDMDRNReports option: %s" % args.addDMDRNReports )
+import sys,os.path
+import csv
+import re
+from ltistdb import *
+import datetime
+
+def populateCompanies (companiesFileName):
+   fh = open(companiesFileName,'r')
+   try:
+      reader = csv.DictReader(fh)
+      print "Starting populate DB with companies, {} companies to go...".format(reader.line_num)
+      i=0
+      startTime = datetime.datetime.now().replace(microsecond=0)
+      for row in reader:
+         #Search for a ticker in the DB
+         if companyExist(row['Ticker']):
+            print "Trying to insert company with ticker, which is already exist"
+            print "Ticker - {} , Name - {},".format(row['Ticker'],row['Stock Name'])
+            print "To confirm type - yes, to decline type - no"
+            answer = raw_input()
+            if not re.match('y', answer):
+               print "Will skip this stock"
+               continue
+            else:
+               print "Will add this stock anyway"
+         registerCompany(row['Ticker'],row['Stock Name'])
+         print "Line #{} is parsed".format(i)
+         i=i+1
+
+   finally:
+      endTime = datetime.datetime.now().replace(microsecond=0)
+      print "Time passed : {}".format(endTime - startTime)
+      fh.close()
+
+def main():
+   #Handle arguments
+   parser = argparse.ArgumentParser(description='LTIST - stands for long term investment system test')
+   parser.add_argument('-p','--populateCompanies', help='Optional parameter, if specified, the script will use the list to populate the fundamentals database')
+   parser.add_argument('-a','--addDMDRNReports', action='store_true', help='Optional parameter, if specified, the script will add reports taken from DMDRN database on each company existing in fundamentals database')
+   parser.add_argument('-c','--clearCompaniesDB', action='store_true', help='Optional parameter, if specified, the script will clear companies records from fundamentals database')
+   args = parser.parse_args()
+
+   if args.clearCompaniesDB:
+      clearCompaniesTable()
+   if args.populateCompanies:
+      if not os.path.isfile(args.populateCompanies):
+         sys.exit("<{}> is not a file. Exiting...".format(args.populateCompanies))   
+      if not args.populateCompanies.endswith('.csv'):
+         sys.exit("<{}> is not a .csv file. Exiting...".format(args.populateCompanies))   
+      #Read file into temporary array
+      populateCompanies(args.populateCompanies)
+
+if __name__ == '__main__':
+   main()
--- ltistdb.py
+++ ltistdb.py
@@ -1,9 +1,18 @@
 import psycopg2
 
-reportTypeToDBNameMapping = {
-	'PE'  : 'ReportPEAnnual',
-	'ROE' : 'ReportROEAnnual'
+#TODO: refactor to class
+#Use connect and cursor function only once per instance
+
+_reportTypeToDBNameMapping = {
+  'PE'  : 'ReportPEAnnual',
+  'ROE' : 'ReportROEAnnual'
 }
+
+def sanitize(text):
+  """The function will prepare the string, to insert to DB"""
+  text = text.replace('\'','\'\'')
+  text = text.replace('\\',' ')
+  return text
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -11,17 +20,20 @@ def connect():
 
 def registerCompany(ticker, name):
     """Adds a company to the foundamentals database.
-  
-    The database assigns a unique serial id number for the company.
+ 
+     The database assigns a unique serial id number for the company.
   
     Args:
       ticker: company ticker (need not be unique)
       name:company name (need not be unique)
     """
+    
     DB = connect()
     cur = DB.cursor()
-    cur.execute("INSERT INTO Companies (name) VALUES (%s,%s)" % ticker, name)
+    cur.execute("INSERT INTO Companies (ticker, name)  VALUES ( '{}', '{}' )".format(sanitize(ticker),sanitize(name)))
     DB.commit()
+    #TODO:think about verbosity
+    print "A new company was added to the DB. Ticker - {} , Name - {}".format(ticker,name)
     DB.close()
 
 def addReport(type, companyId, year, value):
@@ -35,14 +47,59 @@ def addReport(type, companyId, year, value):
       year: for now supported only annual reports
       value: value to insert to report
     """
-    try:
-    	 table = reportTypeToDBNameMapping[type]
-    else
-    	raise ValueError("Incorrect report type, returning...")
+    table = _reportTypeToDBNameMapping[type]
     DB = connect()
     cur = DB.cursor()
-    cmd = "INSERT INTO {1} VALUES ({2},{3},{4})"
+    cmd = """ INSERT INTO {} VALUES ({},{},{})"""
     cur.execute(cmd.format(table,companyId,year,value))
     DB.commit()
     DB.close()
 
+def companyExist(ticker):
+  """Check if company with given ticker exist in the DB
+    Args:
+    ticker: company's symbol
+    Return:
+    True is exist, else False
+  """
+  DB = connect()
+  cur = DB.cursor()
+  cmd = "SELECT COUNT(*) FROM Companies WHERE ticker='{}' GROUP BY ticker"
+  cur.execute(cmd.format(sanitize(ticker)))
+  result = cur.fetchall()
+  if len(result) == 0: return False
+  else: return True
+
+def clearCompaniesTable():
+  """Clear the whole companies table"""
+  DB = connect()
+  cur = DB.cursor()
+  cur.execute("DELETE FROM Companies")
+  DB.commit()
+  DB.close()
+
+def clearReportTable(type):
+  """ Clear report table per given type
+    To see reports types use getReportTypes()
+  """
+  DB = connect()
+  cur = DB.cursor()
+  table = _reportTypeToDBNameMapping[type]
+  cur.execute("DELETE FROM %s" % table)
+  DB.commit()
+  DB.close()  
+
+def getReportTypes():
+  """Returns: supported report types as a dict, where key is report type and value is the table name."""
+  return _reportTypeToDBNameMapping
+
+def UnitTest():
+  registerCompany("AA'A", 'somename')
+  if not companyExist("AA'A"):
+    raise ValueError("Company does not exist, when should")
+  clearCompaniesTable()
+  if companyExist("AA'A"):
+    raise ValueError("Company exist, when shouldn't")
+
+if __name__ == '__main__':
+  UnitTest()
